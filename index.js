e022a16a542fa5eba01de38701cb6d6edefbd47f
matt-mcalister <matt.mcalister93@gmail.com>
2019-06-13 12:00:21 -0400

    finished printString

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // Code your solution here!
 
+function printString(myString) {
+  console.log(myString[0]);
+
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+
+function reverseString(string){
+  if (string.length <= 1){
+    return string
+  } else {
+    let letter = string[string.length-1]
+    return letter + reverseString(string.slice(0,-1))
+  }
+}
